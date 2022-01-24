950d57fbc29c3a6c6ae6f29cfda356ba3158bff4
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2022-01-24 12:20:40 +0200

    initial commit

---
+++ src/main/java/com/example/air_companies_app/AirCompaniesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.air_companies_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirCompaniesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirCompaniesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/air_companies_app/AirCompaniesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.air_companies_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirCompaniesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
